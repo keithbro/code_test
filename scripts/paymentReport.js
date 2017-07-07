4c93bc1cc4412895eec446c81370258fea87cce9
Keith Broughton <keithbro256@gmail.com>
2017-07-07 23:18:29 +0100

    paymentReport

---
+++ scripts/paymentReport.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const { parsePeopleJSON, parseRulesJSON, getPaymentReport, getCompensationForWeek } =
+  require('../lib/calculator')
+
+const moment = require('moment')
+
+const now = moment()
+
+Promise.all(
+  [
+    parsePeopleJSON('./test/data/people.json'),
+    parseRulesJSON('./test/data/rules.json'),
+  ]
+).then((peopleAndRules) => {
+  const [ people, rules ] = peopleAndRules
+  people.forEach((person) => {
+    console.log(person.name)
+    for (date = person.injuryDate.clone(); date.diff(now) < 0; date.add(1, 'weeks')) {
+      console.log(
+        date.format('YYYY/MM/DD'),
+        '$',
+        getCompensationForWeek(person, date, rules).toString()
+      )
+    }
+  })
+})
+
